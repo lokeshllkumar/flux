95f8703142821fb7b6256c94547d034ff6debb99
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2025-06-22 17:20:59 +0530

    refactor: added shell script to setup project

---
+++ setup.sh
@@ -0,0 +1,45 @@
+set -e
+
+if ! command -v go &> /dev/null
+then
+    echo "Error: Go not found in PATH"
+    exit 1
+fi
+
+REPO_URL="https://github.com/lokeshllkumar/flux.git"
+REPO_NAME="load-balancer-project"
+
+echo "Cloning repository: ${REPO_URL}"
+git clone "${REPO_URL}"
+cd "${REPO_NAME}"
+
+go mod tidy
+
+if ! command -v protoc &> /dev/null
+then
+    echo "Error: 'protoc' command not found."
+    exit 1
+fi
+
+if ! command -v protoc-gen-go &> /dev/null || ! command -v protoc-gen-go-grpc &> /dev/null
+then
+    echo "Error: Go gRPC plugins were not found in PATH"
+    exit 1
+fi
+
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+FLUX_GO_CLIENT_DIR="./flux"
+FLUX_PROTO_DIR="${FLUX_GO_CLIENT_DIR}/proto"
+FLUX_GEN_OUTPUT="${FLUX_GO_CLIENT_DIR}/gen"
+FLUX_PROTO_FILE="${FLUX_PROTO_DIR}/service_registry.proto"
+
+mkdir -p "${FLUX_GEN_OUTPUT}"
+
+cd "${FLUX_PROTO_DIR}"
+
+protoc --go_out="${FLUX_GEN_OUTPUT}" --go_opt=paths=source_relative \
+       --go-grpc_out="${FLUX_GEN_OUTPUT}" --go_opt=paths=source_relative \
+       "${FLUX_PROTO_FILE}"
+
+echo "Protobuf stubs have been generated for flux"
